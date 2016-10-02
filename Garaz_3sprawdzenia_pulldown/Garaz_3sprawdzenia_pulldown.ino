c102810c8a5a49c81e442c17ae8af6be65fef6f0
Bodzio <pawel.bodnar1@gmail.com>
2016-10-02 19:56:33 +0200

    02.10.2016

--- Garaz_3sprawdzenia_pulldown/Garaz_3sprawdzenia_pulldown.ino
+++ Garaz_3sprawdzenia_pulldown/Garaz_3sprawdzenia_pulldown.ino
@@ -1,11 +1,11 @@
-#define przekaznik 6 
-#define wlacznik 4 
+#define przekaznik 4 
+#define wlacznik 14 
 
 void setup() {
 
 pinMode(przekaznik, OUTPUT);
 pinMode(wlacznik, INPUT_PULLUP); // dajemy na przycisku podciągnięcie do masy rezystorem
-digitalWrite(przekaznik, HIGH);
+digitalWrite(przekaznik, LOW);
 
 }
 
@@ -18,17 +18,22 @@ void loop() {
     
     if(digitalRead(wlacznik) == LOW)
     {
-      delay(20);
+      delay(10);
       if(digitalRead(wlacznik) == LOW)
       {
-          digitalWrite(przekaznik, LOW);
-          delay(500);
+      delay(10);
+    
+      if(digitalRead(wlacznik) == LOW)
+       {
           digitalWrite(przekaznik, HIGH);
+          delay(500);
+          digitalWrite(przekaznik, LOW);
           while (digitalRead(wlacznik) == LOW);
           delay(1000);
       }
       delay(1000);
       }
       delay(1000);
-      }}
+    }}}
+     
 
